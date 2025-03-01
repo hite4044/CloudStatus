ed50b28eff77f0b0e793e1858ced1a15c31fd09a
hite404 <hite404@qq.com>
2025-03-01 19:25:11 +0800

    feat: 在GUI增加 服务器名 配置项

--- gui/config.py
+++ gui/config.py
@@ -98,7 +98,8 @@ class ConfigPanel(wx.Panel):
             "fix_sep": ["数据空隙修复间隔", float, config.fix_sep],
             "data_dir": ["数据文件夹", str, config.data_dir],
             "data_load_threads": ["数据加载线程数", int, config.data_load_threads],
-            "enable_data_save": ["启用保存数据功能", bool, config.enable_data_save]
+            "enable_data_save": ["启用保存数据功能", bool, config.enable_data_save],
+            "server_name": ["服务器名", str, config.server_name]
         }
         sizer = wx.FlexGridSizer(len(self.config_map) + 1, 2, 5, 5)
         self.SetFont(ft(11))
